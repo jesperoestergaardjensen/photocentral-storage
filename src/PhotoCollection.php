614b3839e75e0509958265444edc5be8f2a069ff
Jesper Østergaard Jensen <jesper@onlinelife.dk>
2021-11-13 13:54:22 +0100

    Simplify PhotoCollection model

--- src/PhotoCollection.php
+++ src/PhotoCollection.php
@@ -10,33 +10,11 @@ class PhotoCollection
 
     private ?string $description;
 
-    /**
-     * @var Photo[] $photo_list
-     */
-    private array $photo_list;
-
-    public function __construct(string $id, string $name, ?string $description, array $photo_list = [])
+    public function __construct(string $id, string $name, ?string $description)
     {
         $this->id = $id;
         $this->name = $name;
         $this->description = $description;
-        $this->photo_list = $photo_list;
-    }
-
-    /**
-     * @param Photo[] $photo_list
-     */
-    public function setPhotoList(array $photo_list): void
-    {
-        $this->photo_list = $photo_list;
-    }
-
-    /**
-     * @return Photo[]
-     */
-    public function getPhotoList(): array
-    {
-        return $this->photo_list;
     }
 
     /**
