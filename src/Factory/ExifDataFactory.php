b5ca1b0dd7791586b64e04401a3281c6b56f9b4a
Jesper Østergaard Jensen <jesper@onlinelife.dk>
2021-11-13 22:46:54 +0100

    Clean up in ExifDataFactory

--- src/Factory/ExifDataFactory.php
+++ src/Factory/ExifDataFactory.php
@@ -14,6 +14,68 @@ class ExifDataFactory
      * @throws PhotoCentralStorageException
      */
     public static function createExifData(string $full_image_file_name_and_path) : ExifData {
+
+        self::ThrowExceptionIfValidationFails($full_image_file_name_and_path);
+
+        // Get a lot of photo information
+        @$exif = exif_read_data($full_image_file_name_and_path);
+
+        if ($exif === false) {
+            throw new PhotoCentralStorageException("Cannot read exif data from file: $full_image_file_name_and_path");
+        }
+
+        // Not all photos seem to have the Orientation info ?!?! - Handle this
+        array_key_exists('Orientation', $exif) ? $orientation = $exif['Orientation'] : $orientation = 0;
+
+        // Set date photo was taken
+        $datePhotoTaken = self::createDatePhotoTaken($exif);
+
+        // Get GPS info
+        if (isset($exif["GPSLatitude"]) && isset($exif['GPSLatitudeRef'])) {
+            $latitude = self::getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+        } else {
+            $latitude = null;
+        }
+
+        if ($latitude !== null && isset($exif["GPSLongitude"]) && isset($exif['GPSLongitudeRef'])) {
+            $longitude = self::getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+        } else {
+            $longitude = null;
+        }
+
+        return new ExifData(
+            $exif['COMPUTED']['Width'],
+            $exif['COMPUTED']['Height'],
+            $orientation,
+            strtotime($datePhotoTaken),
+            $exif['Make'] ?? null,
+            $exif['Model'] ?? null,
+            $latitude,
+            $longitude
+        );
+    }
+
+    private static function createDatePhotoTaken(array $exif): string
+    {
+        if (!isset($exif['DateTimeOriginal']) || $exif['DateTimeOriginal'] == null) {
+            if (!isset($exif['DateTimeDigitized']) || $exif['DateTimeDigitized'] == null) {
+                if (!isset($exif['FileDateTime']) || $exif['FileDateTime'] == null || $exif['FileDateTime'] < 0) {
+                    $datePhotoTaken = null;
+                } else {
+                    $datePhotoTaken = date("Y:m:d H:i:s", $exif['FileDateTime']);
+                }
+            } else {
+                $datePhotoTaken = $exif['DateTimeDigitized'];
+            }
+        } else {
+            $datePhotoTaken = $exif['DateTimeOriginal'];
+        }
+
+        return $datePhotoTaken;
+    }
+
+    private static function ThrowExceptionIfValidationFails(string $full_image_file_name_and_path): void
+    {
         if (is_file($full_image_file_name_and_path) === false) {
             throw new PhotoCentralStorageException("$full_image_file_name_and_path is not a valid file");
         }
@@ -32,55 +94,6 @@ class ExifDataFactory
             throw new PhotoCentralStorageException($full_image_file_name_and_path . " - is not a valid file!");
         } else if (!file_exists($full_image_file_name_and_path)) {
             throw new PhotoCentralStorageException($full_image_file_name_and_path . " - is not a valid path");
-        } else {
-
-            // Get a lot of photo information
-            @$exif = exif_read_data($full_image_file_name_and_path);
-
-            if ($exif === false) {
-                throw new PhotoCentralStorageException("Cannot read exif data from file: $full_image_file_name_and_path");
-            }
-
-            // Not all photos seem to have the Orientation info ?!?! - Handle this
-            array_key_exists('Orientation', $exif) ? $orientation = $exif['Orientation'] : $orientation = 0;
-
-            // Set date photo was taken
-            if (!isset($exif['DateTimeOriginal']) || $exif['DateTimeOriginal'] == null) {
-                if (!isset($exif['DateTimeDigitized']) || $exif['DateTimeDigitized'] == null) {
-                    if (!isset($exif['FileDateTime']) || $exif['FileDateTime'] == null || $exif['FileDateTime'] < 0) {
-                        $datePhotoTaken = null;
-                    } else {
-                        $datePhotoTaken = date("Y:m:d H:i:s", $exif['FileDateTime']);
-                    }
-                } else {
-                    $datePhotoTaken = $exif['DateTimeDigitized'];
-                }
-            } else {
-                $datePhotoTaken = $exif['DateTimeOriginal'];
-            }
-
-            if (isset($exif["GPSLatitude"]) && isset($exif['GPSLatitudeRef'])) {
-                $latitude = self::getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
-            } else {
-                $latitude = null;
-            }
-
-            if ($latitude !== null && isset($exif["GPSLongitude"]) && isset($exif['GPSLongitudeRef'])) {
-                $longitude = self::getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
-            } else {
-                $longitude = null;
-            }
-
-            return new ExifData(
-                $exif['COMPUTED']['Width'],
-                $exif['COMPUTED']['Height'],
-                $orientation,
-                strtotime($datePhotoTaken),
-                $exif['Make'] ?? null,
-                $exif['Model'] ?? null,
-                $latitude,
-                $longitude
-            );
         }
     }
 
